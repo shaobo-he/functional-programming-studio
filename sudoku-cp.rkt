a14c79fd7c4abb0db9e199b84bc08cec4225d077
Shaobo <polarishehn@gmail.com>
2019-01-10 23:09:24 -0700

    finished type-checked eliminate function

--- sudoku-cp.rkt
+++ sudoku-cp.rkt
@@ -5,7 +5,8 @@
 ;; re-implementation of http://norvig.com/sudoku.html
 
 (define-type Options (Listof Natural))
-(define-type Grid (Immutable-HashTable Indices (Listof Options)))
+(define-type Grid (Immutable-HashTable Indices Options))
+(define-type MaybeGrid (U False Grid))
 
 (define-type Unit (Listof Indices))
 
@@ -34,8 +35,64 @@
                            (append (second all)
                                    (third all)))))
       indices))))
-
+#|
 (: board->grid (-> Board Grid))
 (define board->grid
   (λ (board)
     ()))
+|#
+
+(: assign (-> Grid Indices Natural MaybeGrid))
+(define assign
+  (λ (grid indices val)
+    (foldl
+     (λ ([option : Natural] [grid : MaybeGrid])
+       (if (false? grid)
+           #f
+           (eliminate grid indices option)))
+     grid
+     (remove val (hash-ref grid indices)))))
+
+(: eliminate (-> Grid Indices Natural MaybeGrid))
+(define eliminate
+  (λ (grid indices val)
+    (letrec ([apply/eliminate-rule-1 : (-> Grid Natural MaybeGrid)
+                                     (λ (grid val)
+                                       (foldl
+                                        (λ ([peer : Indices] [grid : MaybeGrid])
+                                          (if (false? grid)
+                                              #f
+                                              (eliminate grid peer val)))
+                                        grid
+                                        (get/peers indices)))]
+             [apply/eliminate-rule-2 : (-> Grid MaybeGrid)
+                                     (λ (grid)
+                                       (foldl
+                                        (λ ([unit : Unit] [grid : MaybeGrid])
+                                          (if (false? grid)
+                                              #f
+                                              (let ([possible-indices
+                                                     (filter
+                                                      (λ (indices)
+                                                        (member val (hash-ref grid indices)))
+                                                      unit)])
+                                                (cond
+                                                  [(empty? possible-indices) #f]
+                                                  [(= (length possible-indices) 1) (assign grid (car possible-indices) val)]
+                                                  [else grid]))))
+                                        grid
+                                        (get/units indices)))])
+      (let ([options (hash-ref grid indices)])
+        (if (false? (member val options))
+            grid
+            (let ([eliminated-options (remove val options)])
+              (if (empty? eliminated-options)
+                  #f
+                  (let ([grid-after-rule-1
+                         (if (= (length eliminated-options) 1)
+                             (apply/eliminate-rule-1 grid (car eliminated-options))
+                             grid)])
+                    ;; Maybe Monad wanted!!!
+                    (if (false? grid-after-rule-1)
+                        #f
+                        (apply/eliminate-rule-2 grid-after-rule-1))))))))))
