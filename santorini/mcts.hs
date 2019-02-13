cefd4f02f856e0e8a4a9018ffe353ffe15192180
Shaobo <polarishehn@gmail.com>
2019-02-12 19:06:49 -0700

    fixed a big bug

--- santorini/mcts.hs
+++ santorini/mcts.hs
@@ -22,26 +22,20 @@ data STCrumb = STCrumb { getSearchTree :: SearchTree
 
 type STZipper = (SearchTree, [STCrumb])
 
-backProp' :: STZipper -> STZipper
-backProp' stz@(st, []) = stz -- reached root
-backProp' (st, fcb:rcbs) =
-  let childState = rootLabel st
-      oldState = rootLabel . getSearchTree $ fcb
-      newState = SearchState
-        (getGameState oldState)
-        ((getReward oldState) + (getReward childState))
-        ((getVisits oldState) + (getVisits childState)) in
-    ((Node newState $ (getLT fcb)++[st]++(getRT fcb)), rcbs)
+backProp' :: Int -> STZipper -> STZipper
+backProp' v stz@(st, []) = stz -- reached root
+backProp' v (st, fcb:rcbs) =
+  --let childState = rootLabel st
+    let oldState = rootLabel . getSearchTree $ fcb
+        newState = SearchState
+          (getGameState oldState)
+          ((getReward oldState) + v)
+          ((getVisits oldState) + 1) in
+            ((Node newState $ (getLT fcb)++[st]++(getRT fcb)), rcbs)
 
-backProp :: STZipper -> STZipper
-backProp stz@(st, []) = stz
-backProp stz = backProp $ backProp' stz
-
---addChild :: SearchState -> STZipper -> STZipper
---addChild ss (st, cbs) =
---  let newNode = Node ss []
---      parent = Node (rootLabel st) (newNode:(subForest st)) in
---    (newNode, (STCrumb parent [] (subForest st)):cbs)
+backProp :: Int -> STZipper -> STZipper
+backProp v stz@(st, []) = stz
+backProp v stz = backProp v $ backProp' v stz
 
 isVisited :: SearchState -> Bool
 isVisited (SearchState _ _ visits) = visits > 0
@@ -73,15 +67,18 @@ pickCandidate stz@(st, cbs)
   -- unexpanded node -> expands it and then pick
   | isUnexpanded st =
     let cgs = getGameState . rootLabel $ st in
-      if isWinningPlayer (getBoard cgs) (fst (getPlayers cgs)) then stz else pickCandidate addChildren
+      if isWinningPlayer (getBoard cgs) (snd (getPlayers cgs))
+        then stz
+        else let moves = getValidNextStates cgs in
+          if length moves == 0
+            then stz
+            else pickCandidate $ ((Node (rootLabel st) (mkFreshSTNode <$> moves)), cbs)
   -- fully expanded node -> select best child
   | isFullyExpanded st = pickCandidate getChildByBestUCT
   -- get the first unvisited child
   | otherwise = let (child, l, r) = getUnvisitedChild in
       (child, (STCrumb st l r):cbs)
     where
-      addChildren = let cgs = getGameState . rootLabel $ st in
-       ((Node (rootLabel st) (mkFreshSTNode <$> (getValidNextStates cgs))), cbs)
       getChildByBestUCT = let children = subForest st
                               ucts = getUCT st
                               bestId = snd $ maximumBy (compare `on` fst) $ zip ucts [0..] in
@@ -96,28 +93,36 @@ mctsOneIteration root = let (st@(Node ss children), cbs) = pickCandidate root in
   -- playout on candidate
   do
     (result, _) <- playOut $ (getGameState . rootLabel) st
-    let updatedLeaf = Node (SearchState
+    let v = if result then 1 else 0
+        updatedLeaf = Node (SearchState
                             (getGameState ss)
-                            ((getReward ss)+(if result then 1 else 0))
+                            ((getReward ss)+v)
                             ((getVisits ss)+1)) children
-    return $ backProp (updatedLeaf, cbs)
+    return $ backProp v (updatedLeaf, cbs)
 
 mctsDo :: STZipper -> State StdGen STZipper
 mctsDo root@(st@(Node ss children), cbs)
   | getVisits ss >= 1000 = return root
   | otherwise = mctsOneIteration root >>= mctsDo
 
-mcts :: STZipper -> State StdGen GameState
-mcts root =
+mcts :: GameState -> State StdGen GameState
+mcts gs =
+  do
+    ((Node _ children), _) <- mctsDo $ makeRootZipper gs
+    return $ getGameState . rootLabel $ maximumBy (compare `on` (getVisits . rootLabel)) children where
+    makeRootZipper (GameState _ players board) = (mkFreshSTNode $ GameState 1 players board, [])
+
+mctsShow :: STZipper -> State StdGen String
+mctsShow root =
   do
     root@(st@(Node ss children), cbs) <- mctsDo root
-    return $ getGameState . rootLabel $ maximumBy (compare `on` (getVisits . rootLabel)) children
+    return $ drawTree $ (show . simplifyNode) <$> st where
+      simplifyNode ss = (getReward ss, getVisits ss)
 
 main :: IO()
 main = do
   let gs = GameState 1 (((2,3),(4,4)),((3,5),(2,5))) $
             boardFromList [[0,0,0,0,2],[1,1,2,0,0],[1,0,0,3,0],[0,0,3,0,0],[0,0,0,1,4]]
-      root = mkFreshSTNode gs
-      rootZipper = (root, [])
   --putStrLn $ intercalate "\n" $ map show $ getValidMoves gs
-  putStrLn $ show $ fst $ runState (mcts rootZipper) $ mkStdGen 100
+  putStrLn $ show $ getPlayers . fst $ runState (mcts gs) $ mkStdGen 200
+  --putStrLn $ fst $ runState (mctsShow rootZipper) $ mkStdGen 200
